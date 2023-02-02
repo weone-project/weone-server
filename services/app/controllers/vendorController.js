10f27cef0362cedd50248eeb2312da4587a32af6
KaisarFS <kaisar.10rpl2.2019@gmail.com>
2023-02-02 09:29:44 +0700

    In services/app, remove comment on error handler

--- services/app/controllers/vendorController.js
+++ services/app/controllers/vendorController.js
@@ -1,5 +1,47 @@
-class VendorController {
+const { comparePassword } = require('../helpers/bcrypt');
+const { createToken } = require('../helpers/jwt');
+const { Vendor } = require('../models')
 
+class VendorController {
+    static async registerVendor(req, res, next) {
+        console.log('masuk --');
+        try {
+            const { name, email, password, phoneNumber, address, vendorImgUrl } = req.body
+            const newVendor = await Vendor.create({ name, email, password, phoneNumber, address, vendorImgUrl })
+
+            res.status(201).json({
+                id: newVendor.id,
+                name: name,
+                email: email
+            })
+
+        } catch (error) {
+            console.log(error, '<---- error registerVendor');
+            next(error)
+        }
+    }
+
+    static async loginVendor(req, res, next) {
+        try {
+            const { email, password } = req.body
+            console.log(email, password, '<----- email');
+
+            const findVendor = await Vendor.findOne({ where: { email } })
+            if (!findVendor) throw { name: 'Invalid login' }
+
+            const compareUserPassword = comparePassword(password, findVendor.password)
+            if (!compareUserPassword) throw { name: 'Invalid login' }
+
+            const payload = { id: findVendor.id }
+            const access_token = createToken(payload)
+
+            res.status(200).json({ access_token })
+
+        } catch (error) {
+            console.log(error, '<----- error login vendor');
+            next(error)
+        }
+    }
 }
 
 module.exports = VendorController
--- services/app/routes/vendor.js
+++ services/app/routes/vendor.js
@@ -3,6 +3,8 @@ const router = require('express').Router()
 
 
 // Your route here
+router.post('/register', VendorController.registerVendor)
+router.post('/login', VendorController.loginVendor)
 
 
 module.exports = router
