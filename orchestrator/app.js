6efb67b889dbfde49360644515e3f3121c1ef749
KaisarFS <kaisar.10rpl2.2019@gmail.com>
2023-02-02 11:34:37 +0700

    In server/orchestrator, binding apollo typeDefs & resolvers with app.js

--- orchestrator/app.js
+++ orchestrator/app.js
@@ -6,23 +6,26 @@ const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const port = process.env.PORT || 4000
 
-const { typeDefs, resolvers } = require('./schemas/categorySchema');
+const { typeDefs: categoryTypeDefs, resolvers: categoryResolvers } = require('./schemas/categorySchema')
+const { typeDefs: vendorTypeDefs, resolvers: vendorResolvers } = require('./schemas/vendorSchema')
+const { typeDefs: productTypeDefs, resolvers: productTypeDefs } = require('./schemas/productSchema')
+const { typeDefs: orderTypeDefs, resolvers: productResolvers, orderResolvers } = require('./schemas/orderSchema')
+const { typeDefs: invitationTypeDefs, resolvers: invitationResolvers } = require('./schemas/invitationSchema')
+
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs: [categoryTypeDefs, vendorTypeDefs, productTypeDefs, orderTypeDefs, invitationTypeDefs],
+    resolvers: [categoryResolvers, vendorResolvers, productResolvers, orderResolvers, invitationResolvers],
     introspection: true
 });
 
 async function startServer() {
     try {
-        const { url } = await startStandaloneServer(server, {
-            listen: { port },
-        })
+        const { url } = await startStandaloneServer(server, { listen: { port } })
         console.log(`🚀  Server ready at: ${url}`);
 
     } catch (error) {
-        console.log(error, '<---- error startServer');
+        console.log(error, '<---- error startServer orchestrator');
     }
 }
 
