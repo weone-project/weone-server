3c04c067f8f6c43cc3b6f3a86722b83382487ce4
hidayattresnadi <hidayattresnadi@gmail.com>
2023-02-02 17:50:07 +0700

    testimony created

--- services/app/routes/index.js
+++ services/app/routes/index.js
@@ -3,12 +3,16 @@ const category = require('./category')
 const invitation = require('./invitation')
 const product = require('./product')
 const vendor = require('./vendor')
+const testimony = require('./testimony')
 const router = require('express').Router()
+
+
 
 router.use('/users', user)
 router.use('/categories', category)
 router.use('/invitations', invitation)
 router.use('/products', product)
 router.use('/vendors', vendor)
+router.use('/testimonies', testimony)
 
 module.exports = router
