12273a233cad78867777deecd074ecb061ad88ca
KaisarFS <kaisar.10rpl2.2019@gmail.com>
2023-02-02 15:46:24 +0700

    In server/orchestra, feat: create, delete, get product

--- orchestrator/schemas/vendorSchema.js
+++ orchestrator/schemas/vendorSchema.js
@@ -2,19 +2,77 @@ const redis = require('../config/redis')
 const axios = require('axios')
 const BASE_URL = 'http://localhost:4002'
 
-// const vendorTypeDefs = `#GraphQL
-//     type disini {
+const vendorTypeDefs = `#GraphQL
+    type Vendor {
+        id: ID
+        name: String
+        email: String
+        password: String
+        phoneNumber: String
+        address: String
+        vendorImgUrl: String
+    }
 
-//     }
-// `
+    input VendorForm {
+        name: String
+        email: String
+        password: String
+        phoneNumber: String
+        address: String
+        vendorImgUrl: String
+    }
 
-// const vendorResolvers = {
-//     Query: {
+    type Query {
+        getVendors: [Vendor]
+    }
 
-//     },
-//     Mutation: {
+    type Mutation {
+        createVendor(form: VendorForm): Vendor
+    }
+`
 
-//     }
-// }
+const vendorResolvers = {
+    Query: {
+        getVendors: async () => {
+            try {
+                const cache = await redis.get('get:vendors')
+                if(cache) {
+                    return JSON.parse(cache)
+                } else {
+                    const { data } = await axios({
+                        method: 'get',
+                        url: BASE_URL + '/vendors'
+                    })
 
-// module.exports = { vendorTypeDefs, vendorResolvers }
+
+                    await redis.set('get:vendors', JSON.stringify(data))
+                    return data
+                }
+            } catch (error) {
+                console.log(error, '<---- error getVendors schema');
+                throw error
+            }
+        }
+    },
+    Mutation: {
+        createVendor: async (_, args) => {
+            console.log('masukkk bs', args);
+            try {
+                const { data } = await axios({
+                    method: "post",
+                    url: `${BASE_URL}/vendors`,
+                    data: args.form,
+                });
+                console.log(data, '==== INI DATA');
+
+                // await redis.del("get:vendors");
+                return data;
+            } catch (error) {   
+                console.log(error, '<--- error createVendor orches');
+                throw error;
+            }
+        },
+    }
+}
+
+module.exports = { vendorTypeDefs, vendorResolvers }
